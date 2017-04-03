7b8e567ecc0548237d12b3c2a2ab0c5787118949
Claus Matzinger <claus.matzinger+kb@gmail.com>
2017-04-03 22:32:02 +0200

    fixup

--- eden/capture/server.py
+++ eden/capture/server.py
@@ -11,7 +11,7 @@ class FrameReader:
         self.source = source
 
     def read(self):
-        return (int(bool(any(self.source.last_frame))), source.last_frame)
+        return (int(any(self.source.last_frame)), self.source.last_frame)
 
 
 def get_handler(frame_reader):
